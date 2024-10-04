209076b17a3efd7e708aaacb6145a750baf1b88f
Harold <haroldmartinez_5@hotmail.com>
2024-10-04 00:32:45 -0500

    QRBanner in progress for fastdeliverypage

--- src/app/[locale]/fast-delivery/components/QRBanner.tsx
+++ src/app/[locale]/fast-delivery/components/QRBanner.tsx
@@ -2,30 +2,27 @@ import Button from "@/shared/components/Button";
 import Image from "next/image";
 import React from "react";
 import QR from "../../../../../public/images/fastdeliverypage/QR.png";
+import AppleSvg from "@/svgs/AppleSvg";
+import PlayStoreSvg from "@/svgs/PlayStoreSvg";
+import DescargarApkSvg from "@/svgs/DownloadApkSvg";
 
 const QRBanner = () => {
   return (
     <div className="flex mt-16 mb-16 justify-center">
-      <div className="bg-cyan-gradient w-full h-[450px] md:h-[330px] rounded-2xl flex flex-col md:flex-row md:justify-evenly items-center overflow-hidden relative p-6">
-        <div className="flex flex-col gap-y-3    md:justify-normal lg:justify-normal">
-          <h1 className="text-black font-bold text-2xl w-5/12">
+      <div className="bg-cyan-gradient w-full h-[490px] py-5 md:h-[330px] rounded-2xl flex flex-col md:flex-row justify-center items-center overflow-hidden relative p-6">
+        <div className="flex flex-col gap-y-3 items-center text-center md:items-start md:text-left">
+          <h1 className="text-black font-bold text-2xl w-6/12 ">
             Descarga la App para iOS & Android y compra ahora tu sim
           </h1>
-          <div className="flex gap-x-2 justify-center md:justify-normal lg:justify-normal">
-            <h1 className="text-white text-xl">$650.00</h1>
-            <h1 className="text-red-500 text-xl line-through">$750.00</h1>
+          <div className="flex gap-x-4 mt-2">
+            <AppleSvg />
+            <PlayStoreSvg />
           </div>
-          <div className="w-full flex justify-center md:justify-normal lg:justify-normal ">
-            <Button
-              rounded="full"
-              customStyles="bg-black"
-              size="medium"
-              intent="solid"
-            >
-              Comprar Ahora
-            </Button>
-          </div>
+          <h1 className="font-semibold mt-4">
+            O Escanea el código QR con tu cámara
+          </h1>
         </div>
+
         <Image
           width={220}
           height={220}
@@ -33,7 +30,7 @@ const QRBanner = () => {
           title="Image"
           src={QR}
           alt="Image"
-          className="mt-4 translate-y-[0px] md:mt-0 xl:translate-y-[50px] md:translate-y-[50px]"
+          className="mt-6 md:mt-0 md:translate-y-[50px]"
         />
       </div>
     </div>
