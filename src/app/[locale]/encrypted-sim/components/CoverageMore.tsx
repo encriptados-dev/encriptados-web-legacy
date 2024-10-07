1e3c885edc26da328ebfdeb5ea94cc8dd28ecd42
Harold Martinez <haroldmartinez@MacBook-Air-de-Harold.local>
2024-10-07 00:11:11 -0500

    Progress in order of my component

---
+++ src/app/[locale]/encrypted-sim/components/CoverageMore.tsx
@@ -0,0 +1,17 @@
+import { useTranslations } from "next-intl";
+
+const CoverageMore = () => {
+  const t = useTranslations("EncryptedSimPage");
+  return (
+    <>
+      <h1 className="text-center mt-12 text-2xl font-bold bg-gradient-to-r from-black to-[#31CADD] bg-clip-text text-transparent">
+        {t("coverageTitle")}
+      </h1>
+      <div className="flex justify-center mt-4">
+        <p className="text-center w-5/12">{t("coverageDescription")}</p>
+      </div>
+    </>
+  );
+};
+
+export default CoverageMore;
--- src/app/[locale]/encrypted-sim/components/EncryptedSimPage.tsx
+++ src/app/[locale]/encrypted-sim/components/EncryptedSimPage.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import BannerConect from "./BannerConnect";
+import { useTranslations } from "next-intl";
+import CoverageMore from "./CoverageMore";
 
 const EncryptedSimPage = () => {
+  const t = useTranslations("EncryptedSimPage");
   return (
-    <div className="px-2 2xl:px-96 xl:px-10 lg:px-0 mt-11 w-full flex justify-center ">
-      <BannerConect />
-    </div>
+    <>
+      <div className="px-2 2xl:px-96 xl:px-10 lg:px-0 mt-11 w-full flex justify-center ">
+        <BannerConect />
+      </div>
+
+      <div className="px-2 2xl:px-96 xl:px-10 lg:px-0 mt-11 w-full justify-center">
+        <CoverageMore />
+      </div>
+      <div className="px-2 2xl:px-96 xl:px-10 lg:px-0 mt-11 w-full justify-center">
+        <h1>Testing</h1>
+      </div>
+    </>
   );
 };
 
