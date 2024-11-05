b8b4c36b543f1e0f4a52335e1c614d6fcfb5e782
cristofer bolaños scalante <cristoferscalante@gmail.com>
2024-11-05 14:58:26 -0500

    Actualizacion de ourProducts

--- src/app/[locale]/our-products/components/FormOurProducts.tsx
+++ src/app/[locale]/our-products/components/FormOurProducts.tsx
@@ -1,14 +1,15 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
 import BannerOur from "/public/images/our-products/banner-our.png";
 import { useFormContext } from "react-hook-form";
+
 const FormOurProducts: React.FC = () => {
-  const { setValue, register, watch, reset } = useFormContext();
+  const { register, } = useFormContext();
 
   return (
-    <div className="relative min-h-screen w-full bg-[#000] text-white overflow-hidden py-8">
+    <div className="relative w-full bg-[#000] text-white overflow-hidden py-8 ">
       <div className="container mx-auto px-4 py-12 relative z-10">
+        {/* Header Section */}
         <div className="max-w-3xl mx-auto text-center space-y-6 mb-12">
           <h1 className="text-4xl md:text-4xl font-bold tracking-tight max-w-[400px] m-auto">
             Recibe noticias de productos en tu email
@@ -17,7 +18,8 @@ const FormOurProducts: React.FC = () => {
             Registra tu email para recibir nuestro newsletter
           </p>
 
-          <div className="max-w-[360px] mx-auto">
+          {/* Form Section */}
+          <div className="relative max-w-[360px] mx-auto">
             <input
               {...register("mail")}
               type="email"
@@ -25,16 +27,18 @@ const FormOurProducts: React.FC = () => {
               className="w-full h-12 pl-6 pr-32 rounded-full bg-white border-0 text-black text-sm"
               required
             />
-            <button
-              type="submit"
-              className="absolute right-0 h-12 rounded-full bg-[#35CDFB] hover:bg-[#2eb0d8]/90 text-[#085D77] px-6 font-semibold"
-            >
-              Suscribirme
-            </button>
-            <button onClick={() => reset()}>Resetear formulario</button>
+            <div className="absolute top-0 right-0 flex items-center space-x-2">
+              <button
+                type="submit"
+                className="h-12 rounded-full bg-[#35CDFB] hover:bg-[#2eb0d8]/90 text-[#085D77] px-6 font-semibold"
+              >
+                Suscribirme
+              </button>              
+            </div>
           </div>
         </div>
 
+        {/* Image Banner Section */}
         <div className="relative w-full max-w-5xl mx-auto rounded-lg overflow-hidden shadow-2xl">
           <div className="aspect-[16/9] w-full">
             <Image
@@ -48,10 +52,9 @@ const FormOurProducts: React.FC = () => {
         </div>
       </div>
 
-      {/* Decorative elements */}
-      <div className="absolute inset-0 bg-gradient-to-br from-[#000000] to-black" />
-      <div className="absolute bottom-10 left-40 w-[700px] h-[500px] bg-[#01FFC2]/20 rounded-full blur-3xl" />
-      <div className="absolute top-1/2 right-40 w-[700px] h-[500px] bg-[#10B4E7]/20 rounded-full blur-3xl" />
+      {/* Decorative Elements */}      
+      <div className="absolute top-2/3 md:top-1/2 left-40 md:w-[700px] md:h-[500px] w-[200px] h-[150px] bg-[#01FFC2]/20 rounded-full blur-3xl" />
+      <div className="absolute top-2/3 md:top-1/2right-40 md:w-[700px] md:h-[500px] w-[200px] h-[150px] bg-[#10B4E7]/20 rounded-full blur-3xl" />
     </div>
   );
 };
