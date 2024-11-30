58261a72cc4b05f29d737d1117a650a6e32cd113
Harold Martinez <haroldmartinez@MacBook-Air-de-Harold.local>
2024-11-29 19:34:20 -0500

    pay button for recharge implemented

--- src/shared/components/PayButtonRecharge.tsx
+++ src/shared/components/PayButtonRecharge.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import Button from "./Button";
 import { useFormContext } from "react-hook-form";
 import { useRechargeNumber } from "@/features/recharge/mutations/useRechargeNumber";
+import { useToast } from "../context/ToastContext";
 
 const PayButtonRecharge = () => {
   const { handleSubmit } = useFormContext();
 
-  const { recharge } = useRechargeNumber();
+  const { success, error } = useToast();
+
+  const { recharge } = useRechargeNumber({
+    onError: () => {
+      error("No se pudo hacer recarga");
+    },
+    onSuccess: () => {
+      success("Se hizo la recarga exitosamente");
+    },
+  });
   return (
     <Button
       onClick={handleSubmit((data) => {
