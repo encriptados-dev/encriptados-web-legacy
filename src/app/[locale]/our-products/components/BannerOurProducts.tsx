f2915bb5ee58ad416b255eadf828dc091420757c
Harold Martinez <haroldmartinez@MacBook-Air-de-Harold.local>
2024-12-04 13:02:39 -0500

    progress in responsive home

--- src/app/[locale]/our-products/components/BannerOurProducts.tsx
+++ src/app/[locale]/our-products/components/BannerOurProducts.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import React from "react";
 
 const BannerOurProducts = () => {
-  const BannerBackground = "/images/home/banner-home.jpg";
+  const BannerBackground = "/images/home/banner-home-desktop.jpg";
 
   const t = useTranslations("OurProductsPage");
 
@@ -27,10 +27,7 @@ const BannerOurProducts = () => {
       {/* Contenedor del contenido */}
       <div className="absolute inset-0 flex justify-center items-center">
         <div className="rounded-xl p-5 md:p-7 w-full max-w-7xl">
-          <div
-            className="flex flex-col h-full w-full justify-center items-center text-center 
-            lg:items-start lg:text-left lg:justify-start"
-          >
+          <div className="flex flex-col h-full w-full justify-center items-start text-left">
             <div className="text-white space-y-6 max-w-2xl px-4 lg:px-0">
               <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl leading-tight">
                 <span>{t("banner.titleNewConnectWith")}</span>{" "}
@@ -41,10 +38,10 @@ const BannerOurProducts = () => {
                   {t("banner.titleNewInComunication")}
                 </span>
               </h1>
-              <p className="text-justify lg:text-left text-sm sm:text-base lg:max-w-xl">
+              <p className="text-justify text-sm sm:text-base lg:max-w-xl">
                 {t("banner.descriptionNew")}
               </p>
-              <div className="flex flex-wrap gap-4 pt-4 justify-center lg:justify-start">
+              <div className="flex flex-wrap gap-4 pt-4 justify-start">
                 <button className="bg-[#7CD0F9] text-black font-semibold px-6 sm:px-8 py-2 sm:py-3 rounded-full hover:bg-[#6bb8e0] transition-colors">
                   {t("banner.newButtonBuy")}
                 </button>
