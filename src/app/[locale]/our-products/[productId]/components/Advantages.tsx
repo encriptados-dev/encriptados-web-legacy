885da1981ede6a4f4a9873d07a8d24592b64ac2f
Harold Martinez <haroldmartinez@MacBook-Air-de-Harold.local>
2024-12-10 20:42:01 -0500

    progress in advantages

--- src/app/[locale]/our-products/[productId]/components/Advantages.tsx
+++ src/app/[locale]/our-products/[productId]/components/Advantages.tsx
@@ -9,28 +9,29 @@ export default function Advantages() {
   const advantages = currentProduct?.advantages;
 
   return (
-    <div className="bg-black p-6 w-full  ">
-      <div className="bg-black p-6 w-full lg:w-7/12 mx-auto mt-24 mb-24">
+    <div className="bg-black p-6 w-full">
+      <div className="bg-black w-full lg:w-7/12 mx-auto mt-24 mb-24">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {advantages?.map((advantage, index) => (
-            <>
+            <div
+              key={index}
+              className="flex cursor-pointer items-center gap-3 bg-zinc-900/50 rounded-lg p-6 mx-auto w-full hover:bg-zinc-900/70 transition-colors"
+            >
               <div
-                key={index}
-                className="flex  cursor-pointer items-center gap-3 bg-zinc-900/50 rounded-lg p-6 mx-auto w-full hover:bg-zinc-900/70 transition-colors"
+                className="flex justify-center items-center min-w-[30px] min-h-[30px]"
+                style={{ width: 40, height: 40 }}
               >
-                <div className="text-white">
-                  <Image
-                    src={shieldImage}
-                    alt="Shield Icon"
-                    width={30}
-                    height={30}
-                  />
-                </div>
-                <p className="text-white text-base font-medium">
-                  {advantage.title}
-                </p>
+                <Image
+                  src={shieldImage}
+                  alt="Shield Icon"
+                  width={40}
+                  height={40}
+                />
               </div>
-            </>
+              <p className="text-white text-base font-medium">
+                {advantage.title}
+              </p>
+            </div>
           ))}
         </div>
       </div>
