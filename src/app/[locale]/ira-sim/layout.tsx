eb6a5970cd53db2cbb588d57adc5baacc26af577
saorsu13 <ortizsuarezsantiago13@gmail.com>
2025-02-27 01:19:54 -0500

    Cambios en clase ira sim, se agrega faq y se ocultan botones que no se necesitan

--- src/app/[locale]/ira-sim/layout.tsx
+++ src/app/[locale]/ira-sim/layout.tsx
@@ -5,6 +5,7 @@ import HowItWorks from "./components/HowItWorks";
 import BannerCoverage from "@/shared/BannerCoverage";
 import QRBanner from "../fast-delivery/components/QRBanner";
 import { useTranslations } from "next-intl";
+import FaqsBne from "../tim-sim/components/FaqsBne";
 
 interface LayoutProps {
   children: ReactNode;
@@ -22,6 +23,14 @@ export default function Layout({ children }: LayoutProps) {
       <div className="px-2 2xl:px-96 xl:px-10 lg:px-0 ">
         <QRBanner title={t("downloadApp")} />
       </div>
+      <div className="max-w-3xl mx-auto py-20 px-4 md:px-10 lg:px-20">
+        <h1 className="text-center text-2xl md:text-4xl font-bold bg-gradient-to-r from-gray-900 to-blue-500 bg-clip-text text-transparent mb-10 -mt-20">
+          Preguntas <span className="font-normal">frecuentes</span>
+        </h1>
+
+        <FaqsBne />
+      </div>
+
     </>
   );
 }
